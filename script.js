591dde2c4837ed4f35e98bab2d35a20fe13e5c9d
Morena Zilberman Tavares <morenazilberman@gmail.com>
2026-02-27 18:37:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+document
+  .getElementById("btnMinhasIdeias")
+  .addEventListener("click", () => {
+    window.location.href = "Parte2PginaUsurioM.html";
+  });
+
+
+
+
+var iconMinhasIdeias = document.getElementById("iconMinhasIdeias");
+if (iconMinhasIdeias) {
+  iconMinhasIdeias.addEventListener("click", function (e) {
+    // Add your code here
+  });
+}
+
+var botoMagnetContainer = document.getElementById("botoMagnetContainer");
+if (botoMagnetContainer) {
+  botoMagnetContainer.addEventListener("click", function (e) {
+    // Add your code here
+  });
+}
+
+var botoEficinciaContainer = document.getElementById("botoEficinciaContainer");
+if (botoEficinciaContainer) {
+  botoEficinciaContainer.addEventListener("click", function (e) {
+    // Add your code here
+  });
+}
+
+var botoFilantropiaContainer = document.getElementById("botoFilantropiaContainer");
+if (botoFilantropiaContainer) {
+  botoFilantropiaContainer.addEventListener("click", function (e) {
+    // Add your code here
+  });
+}
+
+var botoVerMaisVdeos = document.getElementById("botoVerMaisVdeos");
+if (botoVerMaisVdeos) {
+  botoVerMaisVdeos.addEventListener("click", function (e) {
+    // Add your code here
+  });
+}
+
+var callToActionFilantropia = document.getElementById("callToActionFilantropia");
+if (callToActionFilantropia) {
+  callToActionFilantropia.addEventListener("click", function (e) {
+    // Add your code here
+  });
+}
+
+var callToActionEficincia = document.getElementById("callToActionEficincia");
+if (callToActionEficincia) {
+  callToActionEficincia.addEventListener("click", function (e) {
+    // Add your code here
+  });
+}
+
+var callToActionMagnet = document.getElementById("callToActionMagnet");
+if (callToActionMagnet) {
+  callToActionMagnet.addEventListener("click", function (e) {
+    // Add your code here
+  });
+}
+
+var botoPgDesafio = document.getElementById("botoPgDesafio");
+if (botoPgDesafio) {
+  botoPgDesafio.addEventListener("click", function (e) {
+    // Add your code here
+  });
+}
